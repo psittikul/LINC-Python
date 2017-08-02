8054cb7357fe291c1a480e3822bb005e34b569f7
Patricia <patricia.sitt@gmail.com>
2017-08-02 18:07:43 -0400

    8/2/2017 6:07 p.m. Need to add code to correctly move between sheets and copy needed values

--- getContactLogData.py
+++ getContactLogData.py
@@ -5,4 +5,29 @@ contactLogSheet = wb.get_sheet_by_name('Contact Log Info')
 print(contactLogSheet['A2'].value)
 
 # Cycle through all the rows of our contact log data
-for row in contactLogSheet['A2:E1320']:
+for row in range(2, 1320):
+    print('Row #: ' + str(row))
+    # Go through each cell in the row to get the necessary information
+    for cell in range(1, 5):
+        clientID = contactLogSheet['A'+str(row)].value
+        time = contactLogSheet['C' + str(row)].value
+        service = contactLogSheet['D' + str(row)].value
+        if (service == 'CT & DC'):
+            time = time/2
+    # Now that we've gotten the necessary information from this row,
+    # take our stored values, copy the rest of the necessary information for this client from the Ref tab,
+    # and create a new row in our data set sheet with the complete information set
+    refSheet = wb.get_sheet_by_name('Ref')
+    for refRow in range(2, refSheet.max_row + 1):
+        if refSheet['A'+str(refRow)].value == clientID:
+            print('Client #: ' + str(refSheet['A'+str(refRow)].value), refSheet['D'+str(refRow)].value)
+            if service == refSheet['D'+str(row)].value:
+                print('This client has the same service as this row')
+                testSheet = wb.get_sheet_by_name('Test')
+                testSheet['A'+str(testSheet.max_row+1)].value = clientID
+                testSheet['M'+str(testSheet.max_row+1)].value = time
+                wb.save('LINC Demographics for LVAIC.xlsx')
+        else:
+            print('This is not the correct client')
+    print('---- END OF ROW ----')
+wb.save('LINC Demographics for LVAIC.xlsx')
